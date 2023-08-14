4a610cf292341c9935d0b1d30996ffd8632b6668
bilanoo <blalkhan94@gmail.com>
2023-08-14 17:18:20 +0100

    grid layout completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="board-container">
+      <div className="header">
+        <p>All Boards</p>
+        <p>All Boards</p>
+        <p>All Boards</p>
+        <p>All Boards</p>
+        <p>All Boards</p>
+      </div>
+      <div className="board-header">
+        <p>Platform Launch</p>
+      </div>
+      <div className="board-content">
+        <p>Board Content</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
