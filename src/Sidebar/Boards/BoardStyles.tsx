4a85a2c26ea08d37cd6389f5128546fd1b338b20
bilanoo <blalkhan94@gmail.com>
2023-08-15 18:06:46 +0100

    added selected styling, mapping through data and add new board button.

---
+++ src/Sidebar/Boards/BoardStyles.tsx
@@ -0,0 +1,75 @@
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+} from "@mui/material";
+
+export const BoardList = styled(List)(() => ({
+  "&": {
+    margin: "0",
+    padding: "0",
+  },
+}));
+
+export const EachBoard = styled(ListItem)(() => ({
+  "&": {
+    paddingLeft: "0",
+    paddingTop: "0",
+    paddingBottom: "0",
+    paddingRight: "20px",
+  },
+}));
+
+export const BoardContent = styled(ListItemButton)(() => ({
+  "&": {
+    paddingLeft: "24px",
+    paddingRight: "0",
+    borderRadius: "0 100px 100px 0",
+  },
+  "&:hover": {
+    backgroundColor: "var(--onhover-board)",
+    borderRadius: "0 100px 100px 0",
+  },
+  "&:focus": {
+    backgroundColor: "var(--dark-purple)",
+    color: "var(--white)",
+  },
+  "&&.Mui-selected": {
+    backgroundColor: "var(--dark-purple)",
+    color: "var(--white)",
+  },
+}));
+
+export const IconContent = styled(ListItemIcon)(() => ({
+  "&": {
+    minWidth: "fit-content",
+  },
+}));
+
+export const BoardText = styled(ListItemText)(() => ({
+  "&": {
+    marginLeft: "12px",
+  },
+  ".MuiListItemText-primary": {
+    fontSize: "15px",
+    fontStyle: "normal",
+    fontWeight: "700",
+  },
+}));
+
+export const CreateNewBoard = styled(Button)(() => ({
+  "&": {
+    margin: 0,
+    color: "var(--dark-purple)",
+    justifyContent: "start",
+    paddingLeft: "28px",
+    fontWeight: "700",
+    marginRight: "20px",
+    height: "46.5px",
+    borderRadius: "0 100px 100px 0",
+  },
+}));
--- src/Sidebar/Boards/Boards.tsx
+++ src/Sidebar/Boards/Boards.tsx
@@ -1,9 +1,51 @@
 import "./Boards.css";
-
+import BoardIcon from "../../assets/icon-board.svg";
+import {
+  BoardContent,
+  BoardList,
+  BoardText,
+  CreateNewBoard,
+  EachBoard,
+  IconContent,
+} from "./BoardStyles";
+import boardData from "../../data.json";
+import { useState } from "react";
 export const Boards = () => {
+  const [selected, setSelectedIndex] = useState(1);
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+  };
   return (
     <div className="boards-container">
       <h5>ALL BOARDS (3)</h5>
+
+      <BoardList>
+        {boardData.boards.map((board, index) => (
+          <EachBoard key={board.name}>
+            <BoardContent
+              selected={selected === index}
+              onClick={(event) => handleListItemClick(event, index)}
+            >
+              <IconContent>
+                <img src={BoardIcon} alt="board-icon" />
+              </IconContent>
+              <BoardText primary={board.name} />
+            </BoardContent>
+          </EachBoard>
+        ))}
+      </BoardList>
+
+      <CreateNewBoard
+        startIcon={
+          <img src={BoardIcon} alt="board-icon" className="board-icon" />
+        }
+      >
+        + Create New Board
+      </CreateNewBoard>
     </div>
   );
 };
